65485edcc0cf13656593fcaf37a1a3fbe6c86558
Racer <reaganjoseph26@yahoo.com>
2020-11-27 11:50:27 -0500

    untrack files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+var youTubeKey = config.YT_KEY
+console.log(youTubeKey)
 
+var apiUrl = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=" + youTubeKey
+fetch (apiUrl).then(function (response) {
+    response.json().then(function (data) {
+        console.log(data)
+    })
+});
