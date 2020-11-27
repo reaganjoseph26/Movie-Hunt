450d8e8d262584e7e28ed245e7f1270a758f9850
Racer <reaganjoseph26@yahoo.com>
2020-11-27 15:34:53 -0500

    youtube api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 var youTubeKey = config.YT_KEY
 console.log(youTubeKey)
 
+
 var apiUrl = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=" + youTubeKey
+
+var tubeData = function () {
 fetch (apiUrl).then(function (response) {
     response.json().then(function (data) {
         console.log(data)
     })
 });
+}
+
+tubeData()
